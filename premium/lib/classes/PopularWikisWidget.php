9fe81a0d0ec9623897fb8a354cd2a5bc4a2502e6
Shane-Carelse <skcarelse@gmail.com>
2018-05-18 14:11:40 +0200

    Update PopularWikisWidget.php
    Add option to limit items in widget
    Add the ability to list parent wikis and subwikis in a single level list

--- premium/lib/classes/PopularWikisWidget.php
+++ premium/lib/classes/PopularWikisWidget.php
@@ -16,7 +16,12 @@ class PopularWikisWidget extends WP_Widget {
 		$options = $instance;
 		
 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Popular Wikis', 'wiki') : $instance['title'], $instance, $this->id_base);
+		$max_count = $instance['max-count'];
 		$hierarchical = $instance['hierarchical'];
+		
+		if($max_count < 1) {
+			$max_count = 1000;
+		}
 		
 		if ($hierarchical == 'yes') {
 			$hierarchical = 0;
@@ -28,15 +33,16 @@ class PopularWikisWidget extends WP_Widget {
 		<?php echo $before_widget; ?>
 		<?php echo $before_title . $title . $after_title; ?>
 		<?php
-			$wiki_posts = get_posts(
-					array(
-					'post_parent' => 0,
-					'post_type' => 'incsub_wiki',
-					'orderby' => 'comment_count',
-					'order' => 'DESC',
-					'numberposts' => 100000
-					)
-				);
+			$posts_filter = array(
+				'post_type' => 'incsub_wiki',
+				'orderby' => 'comment_count',
+				'order' => 'DESC',
+				'numberposts' => $max_count
+			);
+			if($hierarchical == 0) {
+				$posts_filter['post_parent'] = 0;
+			}
+			$wiki_posts = get_posts($posts_filter);
 		?>
 			<ul>
 			<?php
@@ -79,41 +85,43 @@ class PopularWikisWidget extends WP_Widget {
 		<?php
     }
     
-    function update($new_instance, $old_instance) {
-    	global $wiki;
+		function update($new_instance, $old_instance) {
+			global $wiki;
 			$instance = $old_instance;
-      $new_instance = wp_parse_args( (array) $new_instance, array( 'title' => __('Popular Wikis', 'wiki'), 'hierarchical' => 'yes') );
-      $instance['title'] = strip_tags($new_instance['title']);
+			$new_instance = wp_parse_args( (array) $new_instance, array( 'title' => __('New Wikis', 'wiki'), 'max-count' => 0, 'hierarchical' => -1) );
+			$instance['title'] = strip_tags($new_instance['title']);
+			$instance['max-count'] = $new_instance['max-count'];
 			$instance['hierarchical'] = $new_instance['hierarchical'];
-      return $instance;
-    }
-    
-    function form($instance) {
-    	global $wiki;
-			$instance = wp_parse_args( (array) $instance, array( 'title' => __('Popular Wikis', 'wiki'), 'hierarchical' => 'yes'));
-			$options = array('title' => strip_tags($instance['title']), 'hierarchical' => $instance['hierarchical']);
+			return $instance;
+		}
 		
-		if ($options['hierarchical'] == 'yes') {
-			$options['hierarchical'] = 0;
-		} else if ($options['hierarchical'] == 'no') {
-			$options['hierarchical'] = 1;
+		function form($instance) {
+			global $wiki;
+		$instance = wp_parse_args( (array) $instance, array( 'title' => __('New Wikis', 'wiki'), 'max-count' => 0, 'hierarchical' => -1));
+		$options = array('title' => strip_tags($instance['title']), 'max-count' => $instance['max-count'], 'hierarchical' => $instance['hierarchical']);
+		
+		if (!isset($options['max-count'])) {
+			$options['max-count'] = 0;
 		}
 		?>
 		<div style="text-align:left">
 				<label for="<?php echo $this->get_field_id('title'); ?>" style="line-height:35px;display:block;"><?php _e('Title', 'wiki'); ?>:<br />
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $options['title']; ?>" type="text" style="width:95%;" />
 				</label>
-				<label for="<?php echo $this->get_field_id('hierarchical'); ?>" style="line-height:35px;display:block;"><?php _e('Levels', 'wiki'); ?>:<br />
-						<select id="<?php echo $this->get_field_id('hierarchical'); ?>" name="<?php echo $this->get_field_name('hierarchical'); ?>" >
-					<?php for ($i=1; $i<5; $i++) { ?>
-					<option value="<?php echo $i; ?>" <?php if ($options['hierarchical'] == $i){ echo 'selected="selected"'; } ?> ><?php _e($i, 'wiki'); ?></option>
-					<?php } ?>
-					<option value="0" <?php if ($options['hierarchical'] == 0){ echo 'selected="selected"'; } ?> ><?php _e('Unlimited', 'wiki'); ?></option>
-						</select>
-					</label>
+			<label for="<?php echo $this->get_field_id('max-count'); ?>" style="line-height:35px;display:block;"><?php _e('Max items', 'wiki'); ?>:<br />
+			<input id="<?php echo $this->get_field_id('max-count'); ?>" name="<?php echo $this->get_field_name('max-count'); ?>" value="<?php echo $options['max-count'] ?>" type="number" />
+			<label for="<?php echo $this->get_field_id('hierarchical'); ?>" style="line-height:35px;display:block;"><?php _e('Levels', 'wiki'); ?>:<br />
+					<select id="<?php echo $this->get_field_id('hierarchical'); ?>" name="<?php echo $this->get_field_name('hierarchical'); ?>" >
+				<?php for ($i=1; $i<5; $i++) { ?>
+				<option value="<?php echo $i; ?>" <?php if ($options['hierarchical'] == $i){ echo 'selected="selected"'; } ?> ><?php _e($i, 'wiki'); ?></option>
+				<?php } ?>
+				<option value="0" <?php if ($options['hierarchical'] == 0){ echo 'selected="selected"'; } ?> ><?php _e('Unlimited', 'wiki'); ?></option>
+				<option value="-1" <?php if ($options['hierarchical'] == -1){ echo 'selected="selected"'; } ?> ><?php _e('Flatten items', 'wiki'); ?></option>
+					</select>
+				</label>
 			<input type="hidden" name="wiki-submit" id="wiki-submit" value="1" />
 		</div>
 		<?php
-    }
+		}
 }
 
